42bdf9a6103df5d909d4a1ce1088b8e4bfdaa7d6
李萍 <1605581286@qq.com>
2019-03-07 15:31:30 +0800

    删除了 bootstrap

--- vue 练习/NODE_SERVER/app.js
+++ vue 练习/NODE_SERVER/app.js
@@ -7,4 +7,17 @@ const server = http.createServer()
 server.on('request',function(req,res){
     const url = req.url
     
+    if(url === '/getscript'){
+        // 拼接一个合法的 JS 脚本，这里拼接的是一个方法的调用
+        var scriptStr = 'show'
+        // res.end 
+        res.end(scriptStr)
+    } else {
+        res.end('404')
+    }
+})
+
+// 指定端口号并启动服务器监听
+server.listen(3000, function(){
+    console.log('server listen at http://27.0.0.0:3000')
 })
